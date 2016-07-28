2542ca3cc76dd1067d3bdad5e8449f8172231875
nkmlombardi <nkmlombardi@gmail.com>
2016-07-28 14:30:16 -0400

    Untracked bower components and dist directory.

--- dist/application/controllers/overview.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('app.controllers')
-    .controller('controllers.overview', OverviewController);
-
-OverviewController.$inject = ['$scope', '$interval'];
-
-function OverviewController($scope) {
-    console.log('Overview controller loaded!')
-}
