21298f1aacd3ba3a60a9856f472dfe9fb2ddb038
nkmlombardi <nkmlombardi@gmail.com>
2016-11-30 15:37:52 -0500

    Can now link real bank accounts and view them

--- source/application/configuration/plaid.config.js
+++ source/application/configuration/plaid.config.js
@@ -20,7 +20,7 @@ function PlaidConfiguration(
             clientName: 'Victory Finance',
             env: 'tartan',
             key: 'a1ec13246c76e782666e1bb0bf8c8d',
-            product: 'auth'
+            product: 'connect'
         },
 
         function success(token) {
@@ -31,30 +31,38 @@ function PlaidConfiguration(
                     Plaid.getAccounts(function(response) {
                         console.log('Plaid Accounts Service Response: ', response.data)
 
-                        // If any accounts were returned by the linked institution
+                        // If an access token was returned
                         if (response.data.length > 0) {
 
                             // Let's not forget that the request returns an array
                             // of access tokens which each contain accounts
                             response.data.forEach(function(tokenResponse) {
-                                tokenResponse.data.forEach(function(account) {
-                                    $rootScope.accounts.push(account)
-                                })
+
+                                // If any accounts were returned by the linked institution
+                                if (tokenResponse.data.length > 0) {
+                                    tokenResponse.data.forEach(function(account) {
+                                        $rootScope.accounts.push(account)
+                                    })
+                                }
                             })
 
                             // Only bother with transactions if accounts were returned
                             Plaid.getTransactions(function(response) {
                                 console.log('Plaid Transactions Service Response: ', response.data)
 
-                                // If transactions were returned, push them to global transactions
+                                // If an access token was returned
                                 if (response.data.length > 0) {
 
                                     // Let's not forget that the request returns an array
                                     // of access tokens which each contain transactions
                                     response.data.forEach(function(tokenResponse) {
-                                        tokenResponse.data.forEach(function(transaction) {
-                                            $rootScope.transactions.push(transaction)
-                                        })
+
+                                        // Only bother if there are transactions
+                                        if (tokenResponse.data.length > 0) {
+                                            tokenResponse.data.forEach(function(transaction) {
+                                                $rootScope.transactions.push(transaction)
+                                            })
+                                        }
                                     })
                                 }
                             })
