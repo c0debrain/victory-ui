fe3e4949d07b58b67d95db71473a84f25642447d
nkmlombardi@gmail.com <nkmlombardi@gmail.com>
2017-01-13 12:21:22 -0500

    Data binding on account component

--- src/app/components/tray/components/sidebar/components/account/account.js
+++ src/app/components/tray/components/sidebar/components/account/account.js
@@ -1,6 +1,14 @@
 import authenticationService from 'services/authentication'
 
 export default {
+    data: () => {
+        return {
+            user: {
+                name: 'Administrator',
+                avatar: 'http://themesanytime.com/startui/demo/img/photo-64-4.jpg'
+            }
+        }
+    },
     methods: {
         logout() {
             authenticationService.logout()
