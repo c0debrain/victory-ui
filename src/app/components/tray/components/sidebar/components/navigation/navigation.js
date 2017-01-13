6bc1e1f04e33a7ecf98a2cc4475527aed0b1b384
nkmlombardi@gmail.com <nkmlombardi@gmail.com>
2017-01-13 13:54:21 -0500

    navigation

--- src/app/components/tray/components/sidebar/components/navigation/navigation.js
+++ src/app/components/tray/components/sidebar/components/navigation/navigation.js
@@ -1,3 +1,20 @@
 export default {
-
+    data: () => {
+        return {
+            menu: [
+                {
+                    name: 'home.index',
+                    title: 'Overview'
+                },
+                {
+                    name: 'datacenters.index',
+                    title: 'Datacenters',
+                    badge: {
+                        type: 'complete',
+                        content: '21'
+                    }
+                }
+            ]
+        }
+    }
 }
